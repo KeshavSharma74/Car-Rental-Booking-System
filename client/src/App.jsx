2d3e7d2e4003ba31d3f3d16d88ccb8e07acbeca7
keshavsharma74 <keshav165p@gmail.com>
2025-08-24 10:42:58 +0530

    Completed Navigation bar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+
+  const [showLogin,setShowLogin]=useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
     <div>
-      <Navbar></Navbar>
+      
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} ></Navbar>} 
+      {showLogin? "show login":"not showlogin"}
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,65 @@
-import React, { useState } from 'react';
 import { assets, menuLinks } from '../assets/assets';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-const Navbar = ({setShowLogin}) => {
-    const location = useLocation();
-    const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
+const Navbar = ({ setShowLogin }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    return (
-        <div onClick={()=>setOpen(false)} className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-1 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
-            
-            <Link to='/'>
-                <img src={assets.logo} alt="logo" className="h-8" />
+  return (
+    <div
+      className={`flex justify-center  text-[0.95rem] border-b border-b-gray-300 ${
+        location.pathname === '/' && 'bg-gray-200'
+      }`}
+    >
+      <div
+        className={`flex items-center w-[1300px] justify-between px-24 py-2 text-gray-600 transition-all`}
+      >
+        {/* Logo */}
+        <Link to='/'>
+          <img src={assets.logo} alt='logo' className='h-8 mr-[12rem]' />
+        </Link>
+
+        {/* Menu Links */}
+            <div
+            className={`flex flex-row items-center gap-8 p-2 text-gray-700 text-[0.95rem] z-50 ${
+                location.pathname === '/' && 'bg-gray-200'
+            }`}
+            >
+          {menuLinks.map((link, index) => (
+            <Link key={index} to={link.path}>
+              {link.name}
             </Link>
-
-            <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}>
-                {
-                    menuLinks.map((link, index) => (
-                        <Link key={index} to={link.path}>
-                            {link.name}
-                        </Link>
-                    ))
-                }
-            </div>
-
-            <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
-                <input type="text" className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" placeholder="Search products" />
-                <img src={assets.search_icon} alt="search" />
-            </div>
-
-            <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-                <button onClick={ () => navigate('/owner')} className="cursor-pointer">Dashboard</button>
-                <button onClick={()=>setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all rounded-lg">Login</button>
-            </div>
-
+          ))}
         </div>
-    );
+
+        {/* Search Bar */}
+        <div className='flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full w-56'>
+          <input
+            type='text'
+            className='py-1.5 w-full bg-transparent outline-none border-gray-300 placeholder-gray-500'
+            placeholder='Search products'
+          />
+          <img src={assets.search_icon} alt='search' />
+        </div>
+
+        {/* Buttons */}
+        <div className='flex items-center gap-6'>
+          <button
+            onClick={() => navigate('/owner')}
+            className='cursor-pointer'
+          >
+            Dashboard
+          </button>
+          <button
+            onClick={() => setShowLogin(true)}
+            className='cursor-pointer px-8 py-2 border-none bg-blue-600 text-white text-[0.91rem] transition-all rounded-lg'
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
