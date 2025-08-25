8d87ea8c8c114ca95899d3b73df5b9f8b374d1f3
keshavsharma74 <keshav165p@gmail.com>
2025-08-25 22:16:06 +0530

    Completed routes and controllers ffor login and register

---
+++ server/controllers/User.controller.js
@@ -0,0 +1,133 @@
+import User from "../models/User.model.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+
+const generateToken = (userId,res) =>{
+    
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn:"7d"
+    });
+
+    if(!token){
+        return res.status(400).json({
+            success:false,
+            message:"Token cannot be generated"
+        })
+    }
+
+    res.cookie("jwt",token,{
+        httpOnly:true,
+        sameSite:"none",
+        secure:true,
+        maxAge:7*24*60*60*1000,
+    })
+
+}
+
+const register = async(req,res)=>{
+
+    const {name,email,password} = req.body;
+
+    try{
+        
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+
+        if(password.length<8){
+            return res.status(400).json({
+                success:false,
+                message:"Password should be atleast 8 characters"
+            })
+        }
+
+        const isPresent = await User.findOne({email});
+
+        if(isPresent){
+           return res.status(400).json({
+                success:false,
+                message:"Email is already registered"
+           }) 
+        }
+
+        const hashedPassword =await  bcrypt.hash(password,10);
+
+        const user = await User.create({
+            name,
+            email,
+            password:hashedPassword
+        });
+
+        generateToken(user._id,res);
+
+        return res.status(200).json({
+            success:true,
+            message:"User registered successfully"
+        }) 
+
+    }
+    catch(error){
+        console.log("Error while registering the User");
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const login = async(req,res)=>{
+
+    const {email,password} = req.body;
+
+    try{
+
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"Email is not registered"
+            })
+        }
+
+        const isCorrectPassword = await bcrypt.compare(password,user.password);
+
+        if(!isCorrectPassword){
+            return res.status(400).json({
+                success:false,
+                message:"Email or Password is incorrect"
+            })
+        }
+
+        generateToken(user._id,res);
+
+        return res.status(200).json({
+            success:true,
+            message:"User LoggedIn successfully"
+        })
+
+    }
+    catch(error){
+        console.log("Error while Logging In the user");
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+
+export {register,login}
--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ import cors from "cors"
 import "dotenv/config"
 import dbConnect from "./config/database.js";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 
 const app=express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use('/api/user',userRouter);
 
 const port=process.env.PORT || 4000;
 
