e6f22364130fae66ed8b99b25af1b5690f03bf95
keshavsharma74 <keshav165p@gmail.com>
2025-08-25 21:36:18 +0530

    Completed User model

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import dbConnect from "./config/database.js";
+import cookieParser from "cookie-parser";
 
 const app=express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const port=process.env.PORT || 4000;
 
